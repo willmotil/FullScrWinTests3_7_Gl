ea50639d65baa2db484ebf7604da279aee3e03e2
willmotil <willmotil@users.noreply.github.com>
2021-03-29 20:25:07 -0400

    Initial commit

---
+++ FullScrWinTests3_7_Gl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FullScrWinTests3_7_Gl
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1()) game.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FullScrWinTests3_7_Gl
+ Monogame modes resize info tests ect.
